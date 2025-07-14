305b1e06b67410a65cef397ae421cabcb6246d98
rofthchild <130942967+rofthchild@users.noreply.github.com>
2025-07-14 07:13:42 -0500

    start Rock-Paper Scissors app

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # javascript-learning
+<h1><a href="https://www.youtube.com/watch?v=ec8vSKJuZTk">Learn JavaScript by Building 7 Games - Full Course</a></h1>
---
+++ rock-paper-scissors/app.js
@@ -0,0 +1,6 @@
+const computerChoise = document.getElementById('computer-choise')
+const userChoice = document.getElementById('user-choise')
+const resultDisplay = document.getAnimations('result')
+
+
+const posssibleChoices = document.querySelectorAll('button')
