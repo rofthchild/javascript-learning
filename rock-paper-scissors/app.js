c79d7f57470ee567a9c7c7907ef32fd44fee1ae9
rofthchild <130942967+rofthchild@users.noreply.github.com>
2025-07-14 07:41:09 -0500

    setup computer choice functionality

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -3,8 +3,28 @@ const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getAnimations('result')
 const posssibleChoices = document.querySelectorAll('button')
 let userChoice
+let computerChoice
 
 posssibleChoices.forEach(posssibleChoice => posssibleChoice.addEventListener('click', (e) => {
     userChoice= e.target.id
     userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
 }))
+
+    function generateComputerChoice() {
+        const randomNumber = Math.floor(Math.random() * 3) + 1 // or you can use possibleChoices.length
+        console.log(randomNumber)
+        if  (randomNumber == 1){
+            computerChoice = 'rock'
+        }
+
+        if  (randomNumber == 2){
+            computerChoice = 'paper'
+        }
+
+        if  (randomNumber == 3){
+            computerChoice = 'scissors'
+        }
+        
+        computerChoiseDisplay.innerHTML = computerChoice
+    }
