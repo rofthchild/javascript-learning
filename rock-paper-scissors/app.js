2b2719dcd249fb1bb72149b092c96bca03aa0d8c
rofthchild <130942967+rofthchild@users.noreply.github.com>
2025-07-14 07:29:19 -0500

    setup user choice functionality

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -1,6 +1,10 @@
-const computerChoise = document.getElementById('computer-choise')
-const userChoice = document.getElementById('user-choise')
+const computerChoiseDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getAnimations('result')
-
-
 const posssibleChoices = document.querySelectorAll('button')
+let userChoice
+
+posssibleChoices.forEach(posssibleChoice => posssibleChoice.addEventListener('click', (e) => {
+    userChoice= e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+}))
